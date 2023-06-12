5d7b097528ac2a8129ccfd540ba7d46d0648b301
Christopher Black <chris@blacktopinteractive.com>
2023-06-12 13:16:23 -0500

    Final code from live solve.

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Salary calculator that collects input and display it on the DOM.
 - [x] Add our HTML, JS, jQuery to the project
 - [x] HTML form to collect employee name
 - [x] HTML table to display employee names
-- [ ] On submit add the employee name to the table
-- [ ] Add other fields into the form
-- [ ] Append additional fields to the page
-- [ ] Create a variable for total monthly and add to that variable on submit
-- [ ] Display total monthly on the DOM
-- [ ] Update styling when the value is over 20,000 monthly
-- [ ] Delete button that removes the row from the table
+- [x] On submit add the employee name to the table
+- [x] Add other fields into the form
+- [x] Append additional fields to the page
+- [x] Create a variable for total monthly and add to that variable on submit
+- [x] Display total monthly on the DOM
+- [x] Update styling when the value is over 20,000 monthly
+- [x] Delete button that removes the row from the table
 
--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,7 @@
 console.log('script sourced');
 // Shortcut for ready
 $(readyNow);
+let monthlyTotal = 0;
 
 function readyNow() {
     console.log('jQuery is working!');
@@ -10,12 +11,69 @@ function readyNow() {
     $('#employee-form').on('submit', onFormSubmit);
     // $('#submit-btn').on('click', onFormSubmit);
     // Listen for 'click' on buttons
+    //     target           event        filter          function
+    $('#employee-data').on('click', '.delete-button', deleteEmployee);
+    // Listen for clicks on the entire employee table, if what was clicked
+    // was '.delete-button', call the deleteEmployee function.
 
+    // Testing monthly total
+    // updateMonthlyTotal(40000);
+    // updateMonthlyTotal(1000000);
+}
+
+function deleteEmployee(event) {
+    console.log(event.target);
+    console.log(this);
+    let salary = $(this).closest('tr').find('.salary-amount').text();
+    console.log('Salary:', salary);
+    updateMonthlyTotal(-salary);
+  // button   td       tr  
+    $(this).parent().parent().remove();
+    // Does the same thing as the above line
+    // $(this).closest('tr').remove();
 }
 
 function onFormSubmit(event) {
+    console.log(event.target);
     event.preventDefault();
     console.log('form!');
     let employeeName = $('#employee-name').val();
+    let employeeLast = $('#employee-last').val();
+    let employeeID = $('#employee-id').val();
+    let employeeTitle = $('#employee-title').val();
+    let employeeSalary = $('#employee-salary').val();
+
     console.log(`Employee: ${employeeName}`);
+    // Access the HTML table body element
+    let employeeTable = $('#employee-data');
+    // Append our data to the table body
+    employeeTable.append(`
+       <tr>
+        <td>${employeeName}</td>
+        <td>${employeeLast}</td>
+        <td>${employeeID}</td>
+        <td>${employeeTitle}</td>
+        <td class="salary-amount">${employeeSalary}</td>
+        <td>
+            <button class="delete-button">
+                Delete
+            </button>
+        </td>
+       </tr>
+    `);
+    updateMonthlyTotal(employeeSalary);
+}
+
+function updateMonthlyTotal(salary) {
+    console.log(salary);
+    let monthly = Number(salary) / 12;
+    monthlyTotal += Math.round(monthly);
+    monthlyTotal = Math.round(monthlyTotal);
+    $('#total-dollar').text(`$${monthlyTotal}`);
+    if (monthlyTotal > 20000) {
+        // .addClass, .removeClass would also work
+        $('#monthly-total').css('background-color', 'red');
+    } else {
+        $('#monthly-total').css('background-color', 'white');
+    } 
 }
